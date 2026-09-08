8cc7d8efe8d021b35b37de734e372ee81a706fbe
JB4000 <jabi0002@stud.ek.dk>
2026-09-08 20:10:46 +0200

    feature: migrate the about page
    Added a HTTP server, an about page handler, a shared layout HTML template,
    an about template and the legacy stylesheet and team image.
    
    Added a test checking the response status, HTML content type,
    mission header of the html, and the team image reference.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
 
-func main () {
-  fmt.Println("Hello world!")
-  }
+	"github.com/GenZM0nks/AscendingMonk/internal/handlers"
+)
+
+// main registers the routes and starts the web server
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /about", handlers.About)
+
+	staticFiles := http.FileServer(http.Dir("web/static"))
+	router.Handle("GET /static/", http.StripPrefix("/static/", staticFiles))
+
+	log.Println("Server listening on http://localhost:8080")
+
+	serverError := http.ListenAndServe(":8080", router)
+	if serverError != nil {
+		log.Fatal(serverError)
+	}
+}
