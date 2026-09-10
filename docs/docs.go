e1b4c49d5e216f7850cf0786a005bc39684286a1
Frede-72 <frjo0005@stud.ek.dk>
2026-09-10 14:47:05 +0200

    ADDED: OpenAPI specs for register GET endpoint

--- docs/docs.go
+++ docs/docs.go
@@ -69,6 +69,29 @@ const docTemplate = `{
                     }
                 }
             }
+        },
+        "/register": {
+            "get": {
+                "description": "Render the Register template to show in the browser.",
+                "produces": [
+                    "text/html"
+                ],
+                "tags": [
+                    "Pages"
+                ],
+                "summary": "Show register.html",
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    },
+                    "500": {
+                        "description": "error",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
         }
     }
 }`
