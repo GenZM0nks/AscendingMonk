bfae7f5ad4647fa496e71ed143297b4446cb779e
bugintroducer69 <masc1001@stud.ek.dk>
2026-09-11 08:58:29 +0200

    Added `/search` route and logic
    - Ported search template to Go
    - Created `SearchData` to reflect data required for searching specifically.
    - Created and registered the `/search` endpoint
    - Added OpenAPI specification documentation for the new endpoint.
    - Test now passes.

--- internal/handlers/search.go
+++ internal/handlers/search.go
@@ -1,7 +1,31 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/GenZM0nks/AscendingMonk/internal/templates"
+)
+
+// Search handles the rendering of the search template on /search.
+//
+// @Summary Show search.html
+// @Description Render the search template to show in the browser.
+// @Produce html
+// @Success 200
+// @Failure 500 {string} string "error"
+// @Tags Pages
+// @Router /search [get]
 func Search(responseWriter http.ResponseWriter, request *http.Request) {
+	pageData := PageData{
+		PageTitle: "Search",
+		Flashes:   nil,
+	}
 
+	searchData := SearchData{
+		Data:          pageData,
+		Query:         request.URL.Query().Get("q"),
+		SearchResults: nil,
+	}
+
+	templates.LoadAndExecuteTemplate("web/templates/search.html", searchData, responseWriter)
 }
